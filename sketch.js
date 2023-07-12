e5ad14c0949b6be5bf001f9330aff81043100415
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-07-11 23:34:59 -0500

    basic functionality, lacks click and hold coloring and slider

--- sketch.js
+++ sketch.js
@@ -16,3 +16,47 @@ while (rowNum <= 16) {
     }
     colNum = 0;
 }
+
+let selectedNum = 0;
+let selectedColor = null;
+let selected = null
+
+function select(e) {
+    if (selectedNum != 0) {
+        const lastSelected = document.getElementsByClassName("selected");
+        lastSelected[0].classList.remove("selected");
+        selected = document.getElementById(e.target.id);
+        selected.classList.add("selected");
+    } else {
+        selected = document.getElementById(e.target.id);
+        selected.classList.add("selected");
+        ++selectedNum;
+    }
+
+    if (selected.classList[0] == "color") {
+        selectedColor = selected.getAttribute('id');
+    }
+    if (selected.id == "eraser") {
+        selectedColor = "white";
+    } else if (selected.id == "clear") {
+        clear();
+        selectedColor = "white";
+    }
+}
+
+function clear() {
+    const grids = document.querySelectorAll(".col");
+    grids.forEach(grid => {
+        grid.style.backgroundColor = "white";
+    })
+    
+}
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", select));
+
+const grids = document.querySelectorAll(".col");
+grids.forEach(grid => grid.addEventListener("click", (e) => {
+    e.target.style.backgroundColor = selectedColor;
+}));
